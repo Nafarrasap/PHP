02513ac466131d75f5b16dd8d25b29ab1b0df43a
Nafarrasap <46410707+Nafarrasap@users.noreply.github.com>
2019-04-30 12:18:06 +0700

    Add files via upload

---
+++ php2/latihan7.php
@@ -0,0 +1,13 @@
+<?php
+$arrBuah = array ("Mangga", "Apel", "Pisang", "Jeruk");
+echo $arrBuah [0]; //mangga
+echo $arrBuah [3]; //Jeruk
+
+$arrwarna = array();
+$arrwarna[] = "Merah";
+$arrwarna[] = "Biru";
+$arrwarna[] = "Hijau";
+$arrwarna[] = "Putih";
+echo $arrwarna [0]; //merah
+echo $arrwarna [2]; //Hijau
+?>
---
+++ php3/latihan3.php
@@ -0,0 +1,9 @@
+>?php
+$user = "Nafarras";
+$pass = "123"
+if ($user == "Nafarras" && $pass == "123"){
+	echo "login Berhasil";
+} else {
+	echo "login Gagal";
+}
+?>
